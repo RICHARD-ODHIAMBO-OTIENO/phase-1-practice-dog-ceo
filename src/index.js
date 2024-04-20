d5670cf9de1a5d4eebaea8b3a09858efa21576b7
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-20 06:35:18 +0300

    Phase-1-practice-dog-ceo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+// Fetching and displaying the dog image
+document.addEventListener('DOMContentLoaded', () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+      .then(response => response.json())
+      .then(data => {
+          data.message.forEach(imageUrl => {
+              const img = document.createElement('img');
+              img.src = imageUrl;
+              document.body.appendChild(img); 
+          });
+      });
+});
+
+//Creating code that fetches and list dogs breeds
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+fetch(breedUrl)
+  .then(response => response.json())
+  .then(data => {
+      const breedsList = document.getElementById('dog-breeds');
+      Object.keys(data.message).forEach(breed => {
+          const li = document.createElement('li');
+          li.textContent = breed;
+          breedsList.appendChild(li);
+      });
+  });
+
+
+  // Changing font color on the click
+  document.querySelectorAll('#dog-breeds li').forEach(li => {
+    li.addEventListener('click', () => {
+        li.style.color = 'red'; // Change to any color you prefer
+    });
+});
+
+
+//Create code that filters breeds on dropdown change
